3d306bec9234049b05b4cafa1b35a68dc1580f63
Jochen Kall <jochen-Kall@gmx.de>
2020-11-28 08:45:01 +0100

    make code checkpatch.pl compliant (almost)
    Signed-off-by: Jochen Kall <jochen-Kall@gmx.de>

--- Safety-signal-source.c
+++ Safety-signal-source.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0-only
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -7,70 +8,63 @@
 //needed for mkfifo function
 #include <sys/types.h>
 #include <sys/stat.h>
-int main()
+int main(void)
 {
-    // creating the communication pipe   
-    const char* Pipe="/tmp/safety-signal-source_to_safety-app";
-    mkfifo(Pipe,0666);
-    int fd=open(Pipe, O_WRONLY);
+	// creating the communication pipe
+	const char *Pipe = "/tmp/safety-signal-source_to_safety-app";
 
-    // Message counter and Buffer for Message to be sent
-    unsigned int MC=0;
-    unsigned char Message[6];
-    
-    const bool test_checksum=false;
-    const bool test_skip_message=false;
+	mkfifo(Pipe, 0666);
+	int fd = open(Pipe, O_WRONLY);
 
-    // creating the control pipe
-    const char* controlpipe="/tmp/signal-source-control-pipe";
-    //const char* controlpipe="./signal-source-control-pipe";
+	// Message counter and Buffer for Message to be sent
+	unsigned int MC = 0;
+	unsigned char Message[6];
 
-    mkfifo(controlpipe,0666);
-    int fd_controlpipe=open(controlpipe, O_RDONLY | O_NONBLOCK);
+	// creating the control pipe
+	const char *controlpipe = "/tmp/signal-source-control-pipe";
 
-    char Controlmessage[1];
-    // 10 for no command given, enter character coming from empty pipes
-    // "1" for break checksum "2" for skip message
-    Controlmessage[0]=10;
+	mkfifo(controlpipe, 0666);
+	int fd_controlpipe = open(controlpipe, O_RDONLY | O_NONBLOCK);
 
-    while (1){
-        Message[0]=1;
-        Message[1] = (MC >> 24) & 0xFF;
-        Message[2] = (MC >> 16) & 0xFF;
-        Message[3] = (MC >> 8) & 0xFF;
-        Message[4] = MC & 0xFF;
-        Message[5] = (Message[0]+Message[1]+Message[2]+Message[3]+Message[4])%256;
-        
-        read(fd_controlpipe,Controlmessage,1);
-        printf("Command code: %i\n", Controlmessage[0]);
+	char Controlmessage[1];
+	// 10 for no command given, enter character coming from empty pipes
+	// "1" for break checksum "2" for skip message
+	Controlmessage[0] = 10;
 
-        // wrong checksum test "1" corresponds to ASCII 49
-        if (Controlmessage[0] == 49)
-        {
-            Controlmessage[0] == 10;
-            Message[5]=random();
-        }        
+	while (1) {
+		Message[0] = 1;
+		Message[1] = (MC >> 24) & 0xFF;
+		Message[2] = (MC >> 16) & 0xFF;
+		Message[3] = (MC >> 8) & 0xFF;
+		Message[4] = MC & 0xFF;
+		Message[5] = (Message[0]+Message[1]+Message[2]+Message[3]+Message[4])%256;
 
-        printf("Sending Message %i\n",MC);
-        printf("%i %i %i %i %i \n",Message[0],Message[1],Message[2],Message[3],Message[4]);
-        fflush(NULL);
+		read(fd_controlpipe, Controlmessage, 1);
+		printf("Command code: %i\n", Controlmessage[0]);
 
-        // skip message test "2" corresponds to ASCII 50
-        if (Controlmessage[0] == 50)
-        {
-            Controlmessage[0] == 10;
-        }
-        else
-        {
-            write(fd,Message,6);
-        }
-        
-        MC+=1;
-        int sleeptime=1000000;
-        usleep(sleeptime);
-    }
-    close(fd);
+		// wrong checksum test "1" corresponds to ASCII 49
+		if (Controlmessage[0] == 49) {
+			Controlmessage[0] == 10;
+			Message[5] = random();
+		}
 
+		printf("Sending Message %i\n", MC);
+		printf("%i %i %i %i %i\n", Message[0], Message[1], Message[2], Message[3], Message[4]);
+		fflush(NULL);
+
+		// skip message test "2" corresponds to ASCII 50
+		if (Controlmessage[0] == 50) {
+			Controlmessage[0] == 10;
+		} else {
+			write(fd, Message, 6);
+		}
+
+		MC += 1;
+		int sleeptime = 1000000;
+
+		usleep(sleeptime);
+	}
+	close(fd);
 
 return 0;
 }
--- safety-app.c
+++ safety-app.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0-only
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -7,85 +8,84 @@
 //needed for system
 #include <stdlib.h>
 
-unsigned int Message_counter=0;
+// As a global variable, Message_counter is initialized to 0
+unsigned int Message_counter;
 
-//E2E check on read message, 
+//E2E check on read message
 bool do_E2Echeck(unsigned char Message[6])
 {
-    // 1 byte yes or no
-    // 4 byte / 1 Integer message counter
-    unsigned char help[4];
-    help[0]=Message[1];
-    help[1]=Message[2];
-    help[2]=Message[3];
-    help[3]=Message[4];
-    //unsigned int count=(unsigned int) *help;
-    //this casting drives me crazy... 
-    unsigned int count=help[3]+help[2]*256+help[1]*256*256+help[0]*256*256*256;
+	// 1 byte yes or no
+	// 4 byte / 1 Integer message counter
+	unsigned char help[4];
 
-    // 1 byte CRC, parity bit
-    unsigned char parity =(unsigned char) Message[5];
-    
-    //checksum check
-    unsigned char computed_parity=(Message[0]+Message[1]+Message[2]+Message[3]+Message[4])%256;
-    if (computed_parity != parity)
-    {
-        printf("Parity miss");
-        return false;
-    } 
+	help[0] = Message[1];
+	help[1] = Message[2];
+	help[2] = Message[3];
+	help[3] = Message[4];
+	//unsigned int count=(unsigned int) *help;
+	//this casting drives me crazy...
+	unsigned int count = help[3]+help[2]*256+help[1]*256*256+help[0]*256*256*256;
 
-    //Message counter too low
-    if (count < Message_counter)
-    {
-        printf("Message Counter too low, expected %i, received %i \n",Message_counter,count);
-        return false;
-    }
+	// 1 byte CRC, parity bit
+	unsigned char parity = (unsigned char) Message[5];
 
-    //Message counter too large, i.e. messages got lost inbetween
-    if (count >= Message_counter+2)
-    {
-        printf("Message Counter too big, expected %i, received %i \n",Message_counter,count);
-        Message_counter=count;
-        return false;
-    }
+	//checksum check
+	unsigned char computed_parity = (Message[0]+Message[1]+Message[2]+Message[3]+Message[4])%256;
 
-    Message_counter+=1;
-    return true; 
-        
+	if (computed_parity != parity) {
+		printf("Parity miss");
+		return false;
+	}
+
+	//Message counter too low
+	if (count < Message_counter) {
+		printf("Message Counter too low, expected %i, received %i\n", Message_counter, count);
+		return false;
+	}
+
+	//Message counter too large, i.e. messages got lost inbetween
+	if (count >= Message_counter+2) {
+		printf("Message Counter too big, expected %i, received %i\n", Message_counter, count);
+		Message_counter = count;
+		return false;
+	}
+
+	Message_counter += 1;
+	return true;
 }
 
-int main()
+int main(void)
 {
-// creating the communication pipe    
-const char* Pipe="/tmp/safety-signal-source_to_safety-app";
-printf("here I am, listening to a pipe\n");
-mkfifo(Pipe,0666);
-int fd=open(Pipe, O_RDONLY);
-//buffer for the message to be read
-unsigned char Message[6];
-while (1) {
-    if (read(fd,Message,6) > 0)
-    {
-        printf("Received Message:%i %i %i %i %i %i \n",Message[0],Message[1],Message[2],Message[3],Message[4],Message[5]);
-        if (do_E2Echeck(Message))
-        {
-            printf("%s\n",Message);
-        }
-        else
-        {
-            printf("What a mess!, SAFESTATE\n");
-            // could just kill the qt app to get the safe state black screen 
-            // killall afbd-cluster-gauges 
-            // does the trick from the shell, lets try to do that from here yep, works
-             system("killall afbd-cluster-gauges");
-        }
-    }
-    int sleeptime=1000000;
-    usleep(sleeptime);
-    printf("here I am, having waited for %f seconds \n",sleeptime / 1000000.0);
-    fflush(NULL);
-}
-//close file again
-close(fd);
-return 0;
+	// creating the communication pipe
+	const char *Pipe = "/tmp/safety-signal-source_to_safety-app";
+
+	printf("here I am, listening to a pipe\n");
+	mkfifo(Pipe, 0666);
+	int fd = open(Pipe, O_RDONLY);
+
+	//buffer for the message to be read
+	unsigned char Message[6];
+
+	while (1) {
+		if (read(fd, Message, 6) > 0) {
+			printf("Received Message:%i %i %i %i %i %i\n", Message[0], Message[1], Message[2], Message[3], Message[4], Message[5]);
+			if (do_E2Echeck(Message)) {
+				printf("%s\n", Message);
+			} else {
+				printf("What a mess!, SAFESTATE\n");
+				// could just kill the qt app to get the safe state black screen
+				// killall afbd-cluster-gauges
+				// does the trick from the shell, lets try to do that from here yep, works
+				system("killall afbd-cluster-gauges");
+			}
+		}
+		int sleeptime = 1000000;
+
+		usleep(sleeptime);
+		printf("here I am, having waited for %f seconds\n", sleeptime / 1000000.0);
+		fflush(NULL);
+	}
+	//close file again
+	close(fd);
+	return 0;
 }
